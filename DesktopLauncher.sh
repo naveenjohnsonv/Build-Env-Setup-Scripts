1a43d1db0fbb1115d0952afec31bf58fc9f969e5
PizzaG <49663808+PizzaG@users.noreply.github.com>
2020-09-15 13:44:59 -0500

    v0.05
    added Home/Roms directory for rom building 
    added Home/Recoveries directory for recovery building
    
    Former-commit-id: bb6e7ab9daaf5c40c749f10d4a388a914d0ebcaa

--- DesktopLauncher.sh
+++ DesktopLauncher.sh
@@ -1,5 +1,9 @@
-mkdir -p ~/Build_Env_Setup && cd ~/Build_Env_Setup
-git clone https://github.com/PizzaG/Build-Env-Setup-Scripts && cd Build-Env-Setup-Scripts
+mkdir -p ~/Build_Env_Setup
+mkdir -p ~/Recoveries
+mkdir -p ~/Roms
+cd ~/Build_Env_Setup
+git clone https://github.com/PizzaG/Build-Env-Setup-Scripts
+cd Build-Env-Setup-Scripts
 
 bash setup/android_build_env.sh
 bash setup/install_android_sdk.sh
