4223144b958566bb5d002ec353ac5a6684bb0650
PizzaG <49663808+PizzaG@users.noreply.github.com>
2020-09-15 12:44:11 -0500

    v0.02
    added making initial directory, going into it, then cloning the setup scripts and running them
    
    Former-commit-id: e5ed6f537e26d044737831f2e50ce8d7e7799f23

--- DesktopLauncher.sh
+++ DesktopLauncher.sh
@@ -1,9 +1,11 @@
+mkdir -p ~/Build_Env_setup && cd ~/Build_Env_setup
 git clone https://github.com/PizzaG/Build-Env-Setup-Scripts
 cd Build-Env-Setup-Scripts
+
 bash setup/android_build_env.sh
 bash setup/install_android_sdk.sh
 
 bash setup/ccache.sh
 bash setup/git.sh
-bash setup/hub.sh
+bash setup/deb-setup.sh
 bash setup/make.sh
