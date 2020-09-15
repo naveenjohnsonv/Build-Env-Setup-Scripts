5ac0f319129e35ad1cfca867d78ccdcd6fe5e3e7
PizzaG <49663808+PizzaG@users.noreply.github.com>
2020-09-15 12:26:00 -0500

    v0.01
    Desktop Launcher to create a folder, download this source and then run all the needed things in it to setup an Android build Environment
    
    Former-commit-id: e5b466cf83dcfa9b753cc780c8588c2caf596972

---
+++ DesktopLauncher.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/PizzaG/Build-Env-Setup-Scripts
+cd Build-Env-Setup-Scripts
+bash setup/android_build_env.sh
+bash setup/install_android_sdk.sh
+
+bash setup/ccache.sh
+bash setup/git.sh
+bash setup/hub.sh
+bash setup/make.sh
