a0136252281498dd2f4210d6b2582c27ca4ba983
PizzaG <49663808+PizzaG@users.noreply.github.com>
2020-09-15 12:50:05 -0500

    v0.04
    combined some commands
    
    Former-commit-id: 13af59f0f1bcb56df48a38ad9d815ccbaa5f4d91

--- DesktopLauncher.sh
+++ DesktopLauncher.sh
@@ -1,6 +1,5 @@
 mkdir -p ~/Build_Env_Setup && cd ~/Build_Env_Setup
-git clone https://github.com/PizzaG/Build-Env-Setup-Scripts
-cd Build-Env-Setup-Scripts
+git clone https://github.com/PizzaG/Build-Env-Setup-Scripts && cd Build-Env-Setup-Scripts
 
 bash setup/android_build_env.sh
 bash setup/install_android_sdk.sh
