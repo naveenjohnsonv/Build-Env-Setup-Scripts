42f60ea9788bc4fd978bbc3d1eabbad539d5d061
PizzaG <49663808+PizzaG@users.noreply.github.com>
2020-09-16 00:27:05 -0500

    v0.09
    More Additions
    Symlink for Python
    
    Former-commit-id: 8f8a7285d3d9491f1c1a62f23147c0a8b99fb924

--- DesktopLauncher.sh
+++ DesktopLauncher.sh
@@ -3,7 +3,7 @@
 
 echo " Custom Linux Android Build Environment Setup Launcher"
 echo " Made By: PizzaG"
-echo " Version: 0.08"
+echo " Version: 0.09"
 
 echo " Loading Setup Launcher..."
 sleep 5
@@ -12,13 +12,17 @@ mkdir -p ~/Android
 mkdir -p ~/Android/Build_Env_Setup
 mkdir -p ~/Android/Recoveries
 mkdir -p ~/Android/Roms
+mkdir -p ~/Android/Kernels
+sudo apt install git aria2 -y
+sudo apt install python2
+sudo apt install python3
+sudo ln /usr/bin/python3 /usr/bin/python
 cd ~/Android/Build_Env_Setup
 git clone https://github.com/PizzaG/Build-Env-Setup-Scripts
 cd Build-Env-Setup-Scripts
 
 bash setup/android_build_env.sh
 bash setup/install_android_sdk.sh
-
 bash setup/ccache.sh
 bash setup/git.sh
 bash setup/deb-setup.sh
@@ -29,5 +33,5 @@ rm -rf ~/Android/Sdk
 
 echo "" Installation Script All Done, Enjoy""
 
-pause
+read
 
