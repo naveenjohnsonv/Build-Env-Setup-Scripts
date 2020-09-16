6166e5b79f019159f98e24116ddedb5795102c0d
PizzaG <49663808+PizzaG@users.noreply.github.com>
2020-09-16 12:55:27 -0500

    v0.10
    Added Recovery Device Treen Generator
    Added and Tweaked Directory Structure
    
    Former-commit-id: c00343f58549ac23e7be74c00e6b952865445ea9

--- DesktopLauncher.sh
+++ DesktopLauncher.sh
@@ -3,19 +3,25 @@
 
 echo " Custom Linux Android Build Environment Setup Launcher"
 echo " Made By: PizzaG"
-echo " Version: 0.09"
+echo " Version: 0.10"
 
 echo " Loading Setup Launcher..."
 sleep 5
 
+sudo apt install git
+sudo apt install android-tools-adb android-tools-fastboot
+sudo apt install cpio
 mkdir -p ~/Android
 mkdir -p ~/Android/Build_Env_Setup
 mkdir -p ~/Android/Recoveries
+cd ~/Android/Recoveries
+git clone https://github.com/SebaUbuntu/TWRP-device-tree-generator
+mv ~/Android/Recoveries/TWRP-device-tree-generator ~/Android/Recoveries/Device-Tree-Generator
 mkdir -p ~/Android/Roms
 mkdir -p ~/Android/Kernels
 sudo apt install git aria2 -y
-sudo apt install python2
-sudo apt install python3
+#sudo apt install python2
+#sudo apt install python3
 sudo ln /usr/bin/python3 /usr/bin/python
 cd ~/Android/Build_Env_Setup
 git clone https://github.com/PizzaG/Build-Env-Setup-Scripts
