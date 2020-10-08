f9c6f748c3e2fd7dab105317ca270403011ad4b4
PizzaG <49663808+PizzaG@users.noreply.github.com>
2020-10-08 16:32:43 -0500

    v0.14
    Fixed a bug where you would get directory errors if you ran the script more than once
    
    Former-commit-id: 5412cccb34a7e1f2ae5d94e8d2ac5e8103925cde

--- DesktopLauncher.sh
+++ DesktopLauncher.sh
@@ -5,11 +5,18 @@
 
 echo " Custom Linux Android Build Environment Setup Launcher"
 echo " Made By: PizzaG"
-echo " Version: 0.13"
-echo " Version Date:10-6-2020"
+echo " Version: 0.14"
+echo " Version Date:10-8-2020"
 
 echo " Loading Setup Launcher..."
 sleep 5
+
+## **BugFix
+echo " Remove Directories For Fresh Install"
+sudo rm -rf ~/Android/Recoveries/TWRP-device-tree-generator
+sudo rm -rf ~/Android/Recoveries/Device-Tree-Generator
+sudo rm -rf ~/Android/SuperR-RomKitchen
+sudo rm -rf ~/Android/SuperR_RomKitchen
 
 ## Create & Setup Directories
 echo " Create & Setup Directories"
@@ -22,8 +29,8 @@ mkdir -p ~/Android/Recoveries
 ## Install & Setup Device Tree Generator
 echo " Install & Setup Device Tree Generator"
 cd ~/Android/Recoveries
-git clone https://github.com/SebaUbuntu/TWRP-device-tree-generator
-mv ~/Android/Recoveries/TWRP-device-tree-generator ~/Android/Recoveries/Device-Tree-Generator
+sudo git clone https://github.com/SebaUbuntu/TWRP-device-tree-generator
+sudo mv ~/Android/Recoveries/TWRP-device-tree-generator ~/Android/Recoveries/Device-Tree-Generator
 
 ## SuperR Rom Kitchen Installer & Setup
 echo " Install & Setup SuperR Rom Kitchen"
@@ -31,11 +38,11 @@ cd ~/Android
 git clone https://github.com/PizzaG/SuperR-RomKitchen.git
 cd ~/Android/SuperR-RomKitchen
 7z x SuperR_RomKitchen.7z.001
-rm -rf SuperR_RomKitchen.7z.001
-rm -rf SuperR_RomKitchen.7z.002
-mv ~/Android/SuperR-RomKitchen ~/Android/SuperR_RomKitchen
+sudo rm -rf SuperR_RomKitchen.7z.001
+sudo rm -rf SuperR_RomKitchen.7z.002
+sudo mv ~/Android/SuperR-RomKitchen ~/Android/SuperR_RomKitchen
 cd ~/Android/SuperR_RomKitchen
-mv ~/Android/SuperR_RomKitchen/SuperR_Launcher.sh ~/Desktop/SuperR_Launcher.sh
+sudo mv ~/Android/SuperR_RomKitchen/SuperR_Launcher.sh ~/Desktop/SuperR_Launcher.sh
 
 ## Install apt Packages & Symlink Python
 echo " Install apt Packages & Symlink Python"
@@ -50,7 +57,7 @@ sudo ln /usr/bin/python3 /usr/bin/python
 ## Install Remaining Depends & Extras
 echo " Install Remaining Depends & Extras"
 cd ~/Android/Build_Env_Setup
-git clone https://github.com/PizzaG/Build-Env-Setup-Scripts
+sudo git clone https://github.com/PizzaG/Build-Env-Setup-Scripts
 cd Build-Env-Setup-Scripts
 bash setup/android_build_env.sh
 bash setup/install_android_sdk.sh
@@ -61,8 +68,8 @@ bash setup/make.sh
 
 ## Cleanup
 echo " Taking Out The Trash & Tidying Up"
-rm -rf ~/Android/Build_Env_Setup
-rm -rf ~/Android/Sdk
+sudo rm -rf ~/Android/Build_Env_Setup
+sudo rm -rf ~/Android/Sdk
 
 echo "" Installation Script All Done, Enjoy""
 
