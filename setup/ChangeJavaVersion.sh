8aa762820e137d998e1640773932bbf39f18d569
PizzaG <49663808+PizzaG@users.noreply.github.com>
2020-10-09 01:51:41 -0500

    v0.01
    Added Desktop Launchers for Java & Python version changer, depending on which versions you need for what you are building
    
    Former-commit-id: b908d47b18cbf64ac954898e812fb3ae5984c684

---
+++ setup/ChangeJavaVersion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Custom Java Version Selector
+## Copyright: 2020 A-Team Digital Solutions
+
+sudo update-alternatives --config java
+
+echo " "
+echo " PRESS ENTER TO EXIT"
+read
---
+++ setup/ChangePythonVersion.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Custom Python Version Selector
+## Copyright: 2020 A-Team Digital Solutions
+
+sudo update-alternatives --config python
+
+echo " "
+echo " PRESS ENTER TO EXIT"
+read
+
