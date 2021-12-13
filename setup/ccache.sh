083857d1f1db36208b8ccde4cb363afc189ee865
naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
2021-12-13 11:51:02 +0000

    setup: fix ccache.sh dependency

--- setup/ccache.sh
+++ setup/ccache.sh
@@ -5,7 +5,7 @@ git clone git://github.com/ccache/ccache.git
 cd ccache || exit 1
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DZSTD_FROM_INTERNET=ON ..
+cmake -DCMAKE_BUILD_TYPE=Release -DZSTD_FROM_INTERNET=ON -DHIREDIS_FROM_INTERNET=ON ..
 make -j"$(nproc)"
 sudo make install
 cd ..
